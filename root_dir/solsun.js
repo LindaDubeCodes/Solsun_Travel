612a1ac9a8a92ea19eb9c72fd49b539a85770e60
cumPirate <crazyrealindividual123@gmail.com>
2022-08-26 17:11:38 +0200

    latest

--- root_dir/solsun.js
+++ root_dir/solsun.js
@@ -1,16 +1,17 @@
-var app = express();
 
-var handlebars = require('handlebars')
-.create({ defaultLayout: 'main' });
-
-app.engine('handlebars', handlebars.engine);
-
-app.set('view engine', 'handlebars');
 
 
 var express = require('express');
 
 var app = express();
+
+
+var handlebars = require('express3-handlebars')
+    .create({ defaultLayout: 'main' });
+
+app.engine('handlebars', handlebars.engine);
+
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000); //we use this line to override the  port by setting an environment value
 //before you start the server
@@ -19,9 +20,7 @@ app.set('port', process.env.PORT || 3000); //we use this line to override the  p
 
 app.get('/', function (req, res) {
     
-    res.type('text/plain');
-
-    res.send('Welcome to Solsun');
+   res.render('home')
 
 });
 
@@ -29,23 +28,25 @@ app.get('/', function (req, res) {
 //this is the about page
 
 app.get('/about', function (req, res) {
-    res.type('text/plain');
-
-    res.send('About Solsun');
+    
+    res.render('about')
 
 });
 
 //custom 404 page
 
 app.use(function (req, res) {
-    res.type('text/plain'); res.status(404); res.send('404 Not Found');
+    res.status(404);
+     res.render('404');
     
 });
 
 //custom 500 page  //cannot reach the server
 
 app.use(function (err, req, res, next) {
-    console.error(err.stack); res, type('type/plain'); res.status(500); res.send('500 - server error');
+    console.error(err.stack);
+     res.status(500); 
+     res.render('500');
 
 });
 
@@ -58,3 +59,4 @@ app.listen(app.get('port'), function () {
         app.get('port') + '; press ctrl+c to exit.'
     );
 });
+
